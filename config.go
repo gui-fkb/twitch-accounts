e43cd500edf0244bee92d856576ee83cf9d57999
Guilherme Frankenberger <nemseialistar@gmail.com>
2024-05-02 21:41:38 -0300

    first commit
    add project main structure

---
+++ config.go
@@ -0,0 +1,6 @@
+package main
+
+var globalConfig = Config{
+	CapSolverKey: "your_captcha_key",
+	ProxyType:    "your_proxy",
+}
---
+++ structs.go
@@ -0,0 +1,6 @@
+package main
+
+type Config struct {
+	CapSolverKey string
+	ProxyType    string
+}
