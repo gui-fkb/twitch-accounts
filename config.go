3a0f0543b9bed3b59053704eb9455538e4a168d9
Guilherme Frankenberger <nemseialistar@gmail.com>
2024-05-06 01:52:34 -0300

    adjust client id bug
    i don't know why i put ".com" at the end... it resulted in the client_id returned into acc creation being nullify

--- config.go
+++ config.go
@@ -5,5 +5,5 @@ var config = Config{
 	Proxy:          "your_proxy",
 	UserAgent:      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
 	EmailDomain:    "ezztt.com",
-	TwitchClientID: "kimne78kx3ncx6brgo4mv6wki5h1ko.com",
+	TwitchClientID: "kimne78kx3ncx6brgo4mv6wki5h1ko",
 }
