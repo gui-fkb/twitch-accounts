18a868d8f9148c376e9aaff93f34d388ee9ab4a0
Guilherme Frankenberger <nemseialistar@gmail.com>
2024-05-09 00:45:58 -0300

    implement clearScreen func to clean the console logs

--- utils.go
+++ utils.go
@@ -2,6 +2,9 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"os/exec"
+	"runtime"
 
 	"github.com/ox-y/GoGmailnator"
 )
@@ -40,5 +43,25 @@ func fastEmailTest() {
 
 	for _, email := range emails {
 		fmt.Printf("From: %s, Subject: %s, Time: %s\n", email.From, email.Subject, email.Time)
+	}
+}
+
+func clearScreen() {
+	// Clear the screen using platform-specific commands
+
+	switch runtime.GOOS {
+	case "linux", "darwin":
+		// For Linux and macOS
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	case "windows":
+		// For Windows
+		cmd := exec.Command("cmd", "/c", "cls")
+		cmd.Stdout = os.Stdout
+		cmd.Run()
+	default:
+		// Unsupported platform
+		fmt.Println("Unsupported platform")
 	}
 }
