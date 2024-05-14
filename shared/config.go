f780f6047d887fd9ff4205494b327429f1918877
Guilherme Frankenberger <nemseialistar@gmail.com>
2024-05-13 22:06:00 -0300

    remove EmailDomain from config as we changed 1secmail to mailnator

--- shared/config.go
+++ shared/config.go
@@ -13,6 +13,5 @@ var Config = Configuration{
 	CapSolverKey:   "your_api_key",
 	Proxy:          "your_proxy",
 	UserAgent:      "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:109.0) Gecko/20100101 Firefox/109.0",
-	EmailDomain:    "ezztt.com",
 	TwitchClientID: "kimne78kx3ncx6brgo4mv6wki5h1ko",
 }
